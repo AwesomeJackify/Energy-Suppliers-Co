e2d6318c9a9a48b29d2b152d05545874aa392073
Jack Gong <jackgong48@gmail.com>
2021-07-31 18:31:13 +1200

    Front end almost done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
   return (
     <div className="app">
       <div className="app__videos">
-        <Video url="https://download1077.mediafire.com/r1i6kjbvlrag/naufbowg5er1qzm/Jalen+Green+s+NBA+Inspiration+%F0%9F%92%AF+shorts_360P.mp4"
+        <Video 
+        url="https://download1077.mediafire.com/r1i6kjbvlrag/naufbowg5er1qzm/Jalen+Green+s+NBA+Inspiration+%F0%9F%92%AF+shorts_360P.mp4"
         channel="@Jalen Green"
         description="Inspirational speech about the NBA"
-        song="Custom track by Jalen Green" likes={199}/>
-        <Video/>
-        <Video/>
-        <Video/>
+        song="Custom track by Jalen Green" 
+        likes={199}/>
       </div>
     </div>
   );
