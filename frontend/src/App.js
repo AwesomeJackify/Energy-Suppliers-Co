5085fa102970bba2b0055b7bc9c65d537215aa75
Jack Gong <jackgong48@gmail.com>
2021-08-01 13:56:44 +1200

    stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,12 @@ import './assets/css/app.css';
 import Video from './components/Video.js';
 import React, {useState, useEffect} from "react"
 import axios from "./components/Axios.js"
-<<<<<<< HEAD
-import ProgressBar from 'react-bootstrap/ProgressBar'
-=======
 import Navbar from './components/Navbar.js';
 import Header from "./components/Header.js";
->>>>>>> c09e2b790d9f464388c412c4795ca7b006e65ba6
+import Timer from "./components/Timer.js";
 
 function App() {
   const [videos, setVideos] = useState([]); 
-  const [progress, setProgress] = useState([]);
 
   useEffect(() => {
     async function fetchPosts() {
@@ -21,21 +17,13 @@ function App() {
       return response;
     }
 
-    async function fetchProgress() {
-      const response = await axios.get('/v2/progress')
-      setProgress(response.data)
-
-      return response;
-    }
-
     fetchPosts();
-    fetchProgress();
   }, [])
 
   return (
     <div className="app">
       <div className="app__videos">
-          <Header/>
+          <Header />
           {videos.map((video) => (
           <Video 
           key = {video.name}
@@ -46,9 +34,10 @@ function App() {
           likes = {video.likes}/>
         ))}
 
-      <ProgressBar className="progressBar" animated now={progress.progress} />
+      
       </div>
       <Navbar />
+      <Timer />
     </div>
   );
 }
